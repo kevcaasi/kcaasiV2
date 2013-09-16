d5ad4ca1cf671edaba8fc735b70c4c4fc6bb13c3
Kevin Caasi <raysurc@hotmail.com>
2013-09-16 16:13:46 -0700

    Foundation to work on website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KcaasiV2::Application.config.secret_token = 'c96d32ceb65c4f68e04d571d0a0fac6a77c597ae332b765c6590d70f276c91d3350a2c1dbe472de9e26a2869205d344748da6ac5e54b006b05f658ecb9ae8a08'
