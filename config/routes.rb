2cc9fbe34f81d078216d25c8126a6e7639b56629
raysurc <raysurc@hotmail.com>
2013-09-17 18:25:23 -0700

    real working baseline for website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 KcaasiV2::Application.routes.draw do
+  get "homepage/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ KcaasiV2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'homepage#home'
 
   # See how all your routes lay out with "rake routes"
 
